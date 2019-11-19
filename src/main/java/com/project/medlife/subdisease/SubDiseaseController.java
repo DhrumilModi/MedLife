0a6e22fd96761352c9279d197c38fbb89d44be51
DhrumilModi <dhrumil789@gmail.com>
2019-11-19 19:14:46 +0530

    api-getByid

--- src/main/java/com/project/medlife/subdisease/SubDiseaseController.java
+++ src/main/java/com/project/medlife/subdisease/SubDiseaseController.java
@@ -3,10 +3,11 @@ package com.project.medlife.subdisease;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin
 @RestController
 public class SubDiseaseController {
 
